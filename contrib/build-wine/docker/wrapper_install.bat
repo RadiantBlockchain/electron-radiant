9f1da276b41a02ca6ce804cfa7fd8145fccc092f
Axel Gembe <derago@gmail.com>
2022-05-13 13:50:12 +0700

    Wine: Reduce hard-coded paths in link wrapper installer
    Instead of hard-coding the version number in the path to the VS tools
    we can use the `VCToolsInstallDir` variable instead.

--- contrib/build-wine/docker/wrapper_install.bat
+++ contrib/build-wine/docker/wrapper_install.bat
@@ -1,12 +1,12 @@
 @echo off
 
-set VCVER=2019
-set VCDIR=c:\Program Files\Microsoft Visual Studio\%VCVER%\BuildTools\VC
+set VCYEAR=2019
+set VCDIR=c:\Program Files\Microsoft Visual Studio\%VCYEAR%\BuildTools\VC
 
 call "%VCDIR%\Auxiliary\Build\vcvarsall.bat" x86
 
-set LINK_PATH=%VCDIR%\Tools\MSVC\14.29.30133\bin\Hostx86\x86\link.exe
-set LINK_REAL=%VCDIR%\Tools\MSVC\14.29.30133\bin\Hostx86\x86\link_real.exe
+set LINK_PATH=%VCToolsInstallDir%\bin\Hostx86\x86\link.exe
+set LINK_REAL=%VCToolsInstallDir%\bin\Hostx86\x86\link_real.exe
 set LINK_REAL_ESC=\\\"%LINK_REAL:\=\\%\\\"
 
 cl /nologo /std:c++17 /Fe:C:\link.exe "/DPROG=\"%LINK_REAL_ESC%\"" /DARG=\"/Brepro\" /EHsc C:\wrapper.cpp /link /Brepro
