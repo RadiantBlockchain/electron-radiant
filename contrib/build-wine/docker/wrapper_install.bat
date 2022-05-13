42f25dd0e7e61b27c25aa3d053fe63c77617b557
Axel Gembe <derago@gmail.com>
2022-05-13 13:55:54 +0700

    Wine: Pass additional flags to reduce junk in the binaries
    This passes the undocumented `/emittoolversioninfo:no` to stop the
    linker from adding the "Rich" header which contains tool versions into
    the file, see
    
    https://developercommunity.visualstudio.com/t/Add-linker-option-to-strip-Rich-stamp/740443
    
    This also adds the undocumented `/emitpogophaseinfo` to stop PGO
    information to end up in the IMAGE_DEBUG_DIRECTORY.
    
    Both of these seem to cause non-reproducible builds.
    
    This also installs another wrapper for `cl.exe` which uses the post
    arguments of the wrapper to pass these flags to the linker.

--- contrib/build-wine/docker/wrapper_install.bat
+++ contrib/build-wine/docker/wrapper_install.bat
@@ -9,9 +9,18 @@ set LINK_PATH=%VCToolsInstallDir%\bin\Hostx86\x86\link.exe
 set LINK_REAL=%VCToolsInstallDir%\bin\Hostx86\x86\link_real.exe
 set LINK_REAL_ESC=\\\"%LINK_REAL:\=\\%\\\"
 
-cl /nologo /std:c++17 /Fe:C:\link.exe "/DPROG=\"%LINK_REAL_ESC%\"" /DARG=\"/Brepro\" "/DARG_POST=\"\"" /EHsc C:\wrapper.cpp /link /Brepro
+cl /nologo /std:c++17 /Fe:C:\link.exe "/DPROG=\"%LINK_REAL_ESC%\"" "/DARG=\"/Brepro /emittoolversioninfo:no /emitpogophaseinfo\"" "/DARG_POST=\"\"" /EHsc C:\wrapper.cpp /link /Brepro
+
+set CL_PATH=%VCToolsInstallDir%\bin\Hostx86\x86\cl.exe
+set CL_REAL=%VCToolsInstallDir%\bin\Hostx86\x86\cl_real.exe
+set CL_REAL_ESC=\\\"%CL_REAL:\=\\%\\\"
+
+cl /nologo /std:c++17 /Fe:C:\cl.exe "/DPROG=\"%CL_REAL_ESC%\"" "/DARG=\"/Brepro\"" "/DARG_POST=\"/link /emittoolversioninfo:no /emitpogophaseinfo\"" /EHsc C:\wrapper.cpp /link /Brepro
 
 move "%LINK_PATH%" "%LINK_REAL%"
 move "C:\link.exe" "%LINK_PATH%"
+
+move "%CL_PATH%" "%CL_REAL%"
+move "C:\cl.exe" "%CL_PATH%"
 
 exit 0
