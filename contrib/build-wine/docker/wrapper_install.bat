4602a02797e4ec6c7741efd02edee6c99a9d0d79
Axel Gembe <derago@gmail.com>
2022-05-13 14:35:38 +0700

    Wine: Use variables for the flags in the wrapper installer
    This also fixes the quotes to be doubled instead of escaped with a
    backslash.

--- contrib/build-wine/docker/wrapper_install.bat
+++ contrib/build-wine/docker/wrapper_install.bat
@@ -5,17 +5,20 @@ set VCDIR=c:\Program Files\Microsoft Visual Studio\%VCYEAR%\BuildTools\VC
 
 call "%VCDIR%\Auxiliary\Build\vcvarsall.bat" x86
 
+set REPRO_ARGS=/Brepro
+set REPRO_ARGS_LINK=/emittoolversioninfo:no /emitpogophaseinfo
+
 set LINK_PATH=%VCToolsInstallDir%\bin\Hostx86\x86\link.exe
 set LINK_REAL=%VCToolsInstallDir%\bin\Hostx86\x86\link_real.exe
 set LINK_REAL_ESC=\\\"%LINK_REAL:\=\\%\\\"
 
-cl /nologo /std:c++17 /Fe:C:\link.exe "/DPROG=\"%LINK_REAL_ESC%\"" "/DARG=\"/Brepro /emittoolversioninfo:no /emitpogophaseinfo\"" "/DARG_POST=\"\"" /EHsc C:\wrapper.cpp /link /Brepro
+cl /nologo /std:c++17 /Fe:C:\link.exe "/DPROG=""%LINK_REAL_ESC%""" "/DARG=""%REPRO_ARGS% %REPRO_ARGS_LINK%""" "/DARG_POST=""""" /EHsc %REPRO_ARGS% C:\wrapper.cpp /link %REPRO_ARGS_LINK%
 
 set CL_PATH=%VCToolsInstallDir%\bin\Hostx86\x86\cl.exe
 set CL_REAL=%VCToolsInstallDir%\bin\Hostx86\x86\cl_real.exe
 set CL_REAL_ESC=\\\"%CL_REAL:\=\\%\\\"
 
-cl /nologo /std:c++17 /Fe:C:\cl.exe "/DPROG=\"%CL_REAL_ESC%\"" "/DARG=\"/Brepro\"" "/DARG_POST=\"/link /emittoolversioninfo:no /emitpogophaseinfo\"" /EHsc C:\wrapper.cpp /link /Brepro
+cl /nologo /std:c++17 /Fe:C:\cl.exe "/DPROG=""%CL_REAL_ESC%""" "/DARG=""%REPRO_ARGS%""" "/DARG_POST=""/link %REPRO_ARGS_LINK%""" /EHsc %REPRO_ARGS% C:\wrapper.cpp /link %REPRO_ARGS_LINK%
 
 move "%LINK_PATH%" "%LINK_REAL%"
 move "C:\link.exe" "%LINK_PATH%"
